8e5edeb0728bd3a74047a082e68ef2703b9420a5
Martin Maurer <martin.maurer@vergleich.org>
2018-03-04 23:54:07 +0100

    train work

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // Imports
 const ListProvider = require('./list.js');
-const parameters = process.argv;
 
 // Main
-const key = parameters[2] || blk;
+const key = process.argv[2] || 'blk';
 const keyListProvider = new ListProvider('de');
-const keyList = keyListProvider.getListOfLicencePlates().then((list) => {
+const keyList = keyListProvider.getList().then((list) => {
     //console.log('list', list);
 })
 
-keyListProvider.getNumberOfPlates().then((size) => {
-    console.log(size);
-});
+// node app.js plate=b
+console.log('kfz app', key);
 
-//  node app.js plate=b
-console.log('kfz', key);
+keyListProvider.getEntriesWithLetter('B').then((matches) => {
+    console.log(matches);
+});
--- list.js
+++ list.js
@@ -3,27 +3,53 @@
 module.exports = class List {
     constructor ({ countryCode = 'de' }) {
         this.countryCode = countryCode;
+        this.list = {};
     }
 
-    getListOfLicencePlates() {
+    getList() {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                const dummyList = {
-                    'B': 'Berlin',
-                    'M': 'München',
-                    'K': 'Köln',
-                    'H': 'Hannover',
-                    'HH': 'Hamburg',
-                    'BLK': 'Burgenlandkreis',
-                }
-                resolve(dummyList);
-            }, 100);
+            // Faux Ajax 
+            //if (Object.keys(this.list).length === 0) {
+                setTimeout(() => {
+                    const dummyList = {
+                        'B': 'Berlin',
+                        'BIT': 'Bitterfeld',
+                        'M': 'München',
+                        'MSH': 'Mannsfeld-Südharz',
+                        'K': 'Köln',
+                        'KA': 'Karlsruhe',
+                        'H': 'Hannover',
+                        'HH': 'Hamburg',
+                        'BLK': 'Burgenlandkreis',
+                        'BIT': 'Bitterfeld',
+                    }
+                    //this.list = dummyList;
+                    resolve(dummyList);
+                }, 100);
+            //} else {
+            //   resolve(this.list);
+            //}
         });
     }
 
-    getNumberOfPlates() {
-        return this.getListOfLicencePlates().then((list) => {
+    getNumberOfEntries() {
+        return this.getList().then((list) => {
             return Object.keys(list).length;
         }); 
+    }
+
+    getEntriesWithLetter(letterString) {
+        return new Promise((resolve, reject) => {
+            const letter = letterString.charAt(0);
+
+            this.getList().then((list) => {
+                const listArray = Object.keys(list);
+                const filteredList = listArray.filter((value) => {
+                    return value.charAt(0) === letter;
+                });
+
+                resolve(filteredList);
+            });
+        });
     }
 }
