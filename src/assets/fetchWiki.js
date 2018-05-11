552154659abce005da95d7b8cb91a53ebdcdf97b
Martin <mr.mmaurer+work@gmail.com>
2018-05-11 23:59:48 +0200

    added basic parsing

--- src/assets/fetchWiki.js
+++ src/assets/fetchWiki.js
@@ -1,7 +1,20 @@
-const wtf = require('wtf_wikipedia');
+const wikiParser = require('wtf_wikipedia');
 
-wtf.fetch('Liste_der_deutschen_Kfz-Kennzeichen,_die_nicht_mehr_ausgegeben_werden', 'de').then((doc) => {
-  const sections = doc.sections();
+const url = 'Liste_der_deutschen_Kfz-Kennzeichen,_die_nicht_mehr_ausgegeben_werden';
 
-  console.log(sections);
+wikiParser.fetch(url, 'de').then((page) => {
+  const sections = page.sections();
+
+  // filter section for alphabet
+  const indexA = sections.findIndex(section => section.data.title === 'A');
+  const indexZ = sections.findIndex(section => section.data.title === 'Z');
+  const entriesAtoZ = sections
+    .map(section => section.data)
+    .filter((_, index) => index >= indexA && index <= indexZ);
+
+  const plateKey = entriesAtoZ[1].lists[0][0].data.fmt.bold;
+  const plateValue = entriesAtoZ[1].lists[0][0].data.text;
+
+  console.log(plateKey[0]);
+  console.log(plateValue);
 });
