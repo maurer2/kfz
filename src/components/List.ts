bfdadea6d9baffd98486317b664d02f1f426ae5e
Martin <mr.mmaurer+work@gmail.com>
2021-08-19 01:03:02 +0100

    Reverted unnecessary List changes

--- src/components/List.ts
+++ src/components/List.ts
@@ -19,7 +19,6 @@ class List implements ListType {
   entries: any[];
   entriesRestore: any[];
   position: number;
-  testList: any[];
 
   constructor() {
     const entriesCurrent = Object.keys(licenceJSON).map(key => {
@@ -61,8 +60,6 @@ class List implements ListType {
     this.entries = [...entriesSorted];
     this.entriesRestore = [...entriesSorted];
     this.position = 0;
-
-    this.testList = [[...entriesSorted][0]];
   }
 
   getCurrentSize() {
@@ -74,10 +71,6 @@ class List implements ListType {
   }
 
   getEntriesWithLetter(letter: string) {
-    if (letter === "") {
-      return this.entriesRestore;
-    }
-
     const filteredEntries = this.entries.filter(entry => entry.startsWith(letter));
 
     return filteredEntries;
@@ -99,19 +92,11 @@ class List implements ListType {
     this.entries = this.entries.filter(entry => entry.hasLetterAtPosition(this.position, letter));
     this.position = this.position + 1;
 
-    return [...this.entries];
+    return this.entries;
   }
 
   resetList() {
-    // this.entries = [...this.entriesRestore];
-
-    // this.entries = this.testList;
-
-    const test = this.getEntriesWithLetter("");
-
-    console.log(test);
-
-    this.entries = this.getEntriesWithLetter("");
+    this.entries = [...this.entriesRestore];
     this.position = 0;
   }
 }
